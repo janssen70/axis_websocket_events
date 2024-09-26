a3ed2515b7d5c5e5166aa946fda189e3b90cb0df
erik <ejanssen@itmatters.nl>
2024-09-26 13:26:48 +0200

    Added support for (unverified) secure connections

--- get_events.py
+++ get_events.py
@@ -2,9 +2,11 @@
 Connect to Axis devices over websocket connection and subscribe to event data
 topics
 
-Example invocation:
+Example invocations:
+
 python3 get_events.py -c 169.254.205.195 -c 169.254.200.32 -u root -p pass -t axis:Device/tnsaxis:IO/Port -t axis:CameraApplicationPlatform/VMD/Camera1ProfileANY
 python3 get_events.py -c 192.168.200.18 -u root -p pass -t onvif:AudioSource/axis:TriggerLevel
+python3 get_events.py -c 192.168.200.18 -u root -p pass -t onvif:VideoAnalytics/axis:TrackEnded
 
 # If you get this:
 #
@@ -26,10 +28,24 @@ import uuid
 import logging
 import argparse
 from datetime import datetime
+import urllib3
+import ssl
+
+urllib3.disable_warnings()
 
 logger = logging.getLogger('websockets')
 logger.setLevel(logging.ERROR)
 logger.addHandler(logging.StreamHandler())
+
+def StandardSSLContext():
+   """
+   Return a SSL context that tells to ignore certificate validity. Maybe not a
+   good idea in general but it serves the testing purpose of this script
+   """
+   ctx = ssl.create_default_context()
+   ctx.check_hostname = False
+   ctx.verify_mode = ssl.CERT_NONE
+   return ctx
 
 #-------------------------------------------------------------------------------
 #
@@ -39,7 +55,7 @@ logger.addHandler(logging.StreamHandler())
 
 class NotificationHandler:
    """
-   Baseclass to offer a bit of structure in handling the various event
+   Baseclass to offer some structure in handling the various event
    responses from the device 
 
    This is not complete at all
@@ -112,14 +128,14 @@ def handle_notification(device, notification):
 #-------------------------------------------------------------------------------
 
 def calculate_digest_response(username, realm, password, uri, method, nonce, nc, cnonce):
-    ha1 = hashlib.md5(f"{username}:{realm}:{password}".encode()).hexdigest()
-    ha2 = hashlib.md5(f"{method}:{uri}".encode()).hexdigest()
+    ha1 = hashlib.md5(f'{username}:{realm}:{password}'.encode()).hexdigest()
+    ha2 = hashlib.md5(f'{method}:{uri}'.encode()).hexdigest()
     response = hashlib.md5(
-        f"{ha1}:{nonce}:{nc}:{cnonce}:auth:{ha2}".encode()).hexdigest()
+        f'{ha1}:{nonce}:{nc}:{cnonce}:auth:{ha2}'.encode()).hexdigest()
     return response
 
 
-async def metadata_websocket_connection(device, topics, username, password, nonce, realm):
+async def metadata_websocket_connection(device, topics, username, password, nonce, realm, secure = False):
    """
    """
    uri = '/vapix/ws-data-stream?sources=events'
@@ -128,18 +144,24 @@ async def metadata_websocket_connection(device, topics, username, password, nonc
    nc = '00000001'
    cnonce = str(uuid.uuid4())
    response = calculate_digest_response(username, realm, password, uri, method, nonce, nc, cnonce)
-   websocket_uri = f'ws://{device}/vapix/ws-data-stream?sources=events'
+   websocket_uri = f'ws{"s" if secure else ""}://{device}{uri}'
    websocket_headers = {
-      "Authorization": f'Digest username="{username}", realm="{realm}", nonce="{nonce}", uri="{uri}", response="{response}", qop=auth, nc={nc}, cnonce="{cnonce}"'
+      'Authorization': f'Digest username="{username}", realm="{realm}", nonce="{nonce}", uri="{uri}", response="{response}", qop=auth, nc={nc}, cnonce="{cnonce}"'
    }
 
    try:
-      async with websockets.connect(websocket_uri, extra_headers=websocket_headers) as websocket:
+      kwargs = {
+        'extra_headers': websocket_headers,
+      }
+      if secure:
+         kwargs['ssl'] = StandardSSLContext()
+
+      async with websockets.connect(websocket_uri, **kwargs) as websocket:
          payload = json.dumps({
-            "apiVersion": "1.0",
-            "method": "events:configure",
-            "params": {
-               "eventFilterList": [{"topicFilter": p} for p in topics]
+            'apiVersion': '1.0',
+            'method': 'events:configure',
+            'params': {
+               'eventFilterList': [{'topicFilter': p} for p in topics]
             }
          })
          await websocket.send(payload)
@@ -161,15 +183,15 @@ async def metadata_websocket_connection(device, topics, username, password, nonc
    except Exception as e:
       print(e)
 
-async def event_listener_task(device, topics, username, password):
+async def event_listener_task(device, topics, username, password, secure = False):
    """
    Enforce a http login so that we get to know digest nonce and realm, which
    we can then use in the websocket connection. Then connect to the websocket
    """
    # url = f'http://{device}/axis-cgi/login.cgi'
-   url = f'http://{device}/axis-cgi/param.cgi?action=list&group=Brand&usergroup=viewer'
+   url = f'http{"s" if secure else ""}://{device}/axis-cgi/param.cgi?action=list&group=Brand&usergroup=viewer'
 
-   response = requests.get(url)
+   response = requests.get(url, verify = False)
    nonce = None
    realm = None
    auth_info = response.headers.get('WWW-Authenticate')
@@ -180,8 +202,8 @@ async def event_listener_task(device, topics, username, password):
    if match:
       realm = match.group(1)
    if nonce and realm:
-      response = requests.get(url, auth=requests.auth.HTTPDigestAuth(username, password))
-      await metadata_websocket_connection(device, topics, username, password, nonce, realm)
+      response = requests.get(url, auth=requests.auth.HTTPDigestAuth(username, password), verify = False)
+      await metadata_websocket_connection(device, topics, username, password, nonce, realm, secure)
    else:
       print('Failed to obtain authentication prerequisites')
 
@@ -193,7 +215,7 @@ async def main(args):
    for c in args.camera:
       tasks.append(
          asyncio.create_task(
-             event_listener_task(c, args.topic, args.user, args.password)
+             event_listener_task(c, args.topic, args.user, args.password, args.secure)
          )
       )
    await asyncio.gather(*tasks)
@@ -204,7 +226,7 @@ async def main(args):
 #
 #-------------------------------------------------------------------------------
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
             description='Websocket test'
@@ -212,16 +234,19 @@ if __name__ == "__main__":
 
     parser.add_argument(
             '-c', '--camera', type=str, action = 'append',
-            help='Hostname/IP address of the device(s) to interact with')
+            help = 'Hostname/IP address of the device(s) to interact with')
     parser.add_argument(
             '-u', '--user', type=str, default='root',
-            help='username to login with (root)')
+            help = 'username to login with (root)')
     parser.add_argument(
             '-p', '--password', type=str, default='pass',
-            help='password to login with (pass)')
+            help = 'password to login with (pass)')
     parser.add_argument(
             '-t', '--topic', type = str, action = 'append',
-            help='Event topic to listen to')
+            help = 'Event topic to listen to')
+    parser.add_argument(
+            '-s', '--secure', action='store_true',
+            help = 'Use secure protocols')
 
     args = parser.parse_args()
     if args.camera is None:
